174da555352b0f8d5ac6d68738a477a71d10202a
stepa <stepaosnova1@gmail.com>
2023-09-04 21:51:09 +0300

    start develop token auth module

--- chess_BE/src/auth/auth.service.ts
+++ chess_BE/src/auth/auth.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { getDataCurrent } from 'src/DataBase';
+import { logindata} from 'src/users/dto/userdata';
 
 @Injectable()
 export class AuthService {
     
+
+    async GiveToken(User:logindata){
+        const doc = (await getDataCurrent('users', User.Email)).doc
+        
+        return {'token':'qwe'}
+    }
 }
