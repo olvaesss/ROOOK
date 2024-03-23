a987fb484b057245770e11cda5c48131799b09d9
Stepan Staravoitau <stepaosnova1@gmail.com>
2024-03-23 14:05:57 +0300

    adding dockerfiles&editing schemas

--- chess_BE/src/main.ts
+++ chess_BE/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.SERVER_PORT
+  const PORT = process.env.SERVER_PORT||3001
   app.enableCors({
     origin:true,
   })
